c3a94938284c9c4809f3413d2f7c172ec9157384
emilknysak <knysak.emil@gmail.com>
2025-08-24 18:31:53 +0200

    fixes

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -1,20 +1,38 @@
-import { env } from "@/env";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, isAxiosError } from 'axios';
+import { env } from './env';
 
 export const api = axios.create({
-  baseURL: env.API_URL,
-  headers: { "Content-Type": "application/json" },
-  withCredentials: true, // ✅ cookies, auth, etc.
+    baseURL: env.EXPO_PUBLIC_API_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
 });
 
-export async function getFetcher<T, P = Record<string, any>>(url: string, params?: P): Promise<T> {
-  const res = await api.get<T>(url, { params });
-  return res.data;
+export async function getFetcher<T, P = Record<string, any>>(
+    url: string,
+    params?: P,
+): Promise<T> {
+    const res = await api.get<T>(url, { params });
+    return res.data;
 }
 
-export async function postFetcher<T, B = unknown>(url: string, body: B): Promise<T> {
-  const res = await api.post<T>(url, body);
-  return res.data;
+export async function postFetcher<TResponse, TRequest>(
+    url: string,
+    body: TRequest,
+): Promise<TResponse> {
+    const res = await api.post<TResponse>(url, body);
+    return res.data;
 }
 
-export type ApiError<T = any> = AxiosError<T>;
+export function extractErrorMessage(error: unknown): string {
+    if (isAxiosError<ApiErrorResponse>(error)) {
+        return error.response?.data.message || error.message;
+    }
+    return 'An unexpected error occurred';
+}
+
+export type ApiErrorResponse = {
+    message: string;
+    code: string;
+};
+
+export type ApiError<T = ApiErrorResponse> = AxiosError<T>;
