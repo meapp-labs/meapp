417a0b71ad8b514978b82d95e4f1168f4ba8f20d
dawid-kozlowski <dknecksnap@gmail.com>
2025-09-30 20:10:05 +0200

    when the intern gets access to the codebase

--- client/services/auth.ts
+++ client/services/auth.ts
@@ -5,26 +5,26 @@ import { LoginType, RegisterType } from '@/validation/userValidation';
 import { useMutation } from '@tanstack/react-query';
 import { router } from 'expo-router';
 
-export function useRegisterUser({ onSuccess }: { onSuccess: () => void }) {
+export function useRegisterUser
+    ({ onSuccess }: { onSuccess: () => void }) {
     return useMutation<string, ApiError, RegisterType>({
-        mutationFn: (body) => postFetcher(Keys.Mutation.REGISTER, body),
+
+
+
+        mutationFn:
+            (body) => postFetcher(Keys.Mutation.REGISTER, body),
         onSuccess,
     });
 }
 
-export function useLoginUser() {
+export function
+    useLoginUser() {
     return useMutation<string, ApiError, LoginType>({
         mutationFn: (body) => postFetcher(Keys.Mutation.LOGIN, body),
     });
 }
 
-export function useLogoutUser({ onSuccess }: { onSuccess: () => void }) {
-    return useMutation<string, ApiError>({
-        mutationFn: () => postFetcher<string, void>(Keys.Mutation.LOGOUT),
-        onSuccess: () => {
-            queryClient.clear();
-            router.replace('/login');
-            onSuccess();
-        },
-    });
+export function
+    seLogoutUser() {
+    return console.log('zdupcyło się!!');
 }
